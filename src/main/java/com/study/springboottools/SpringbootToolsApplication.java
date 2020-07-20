3973bccc4146fc2e3684dc78a8fa8477029f892b
ssliu <ssliu@chindigg.com>
2020-07-20 11:42:15 +0800

    init

---
+++ src/main/java/com/study/springboottools/SpringbootToolsApplication.java
@@ -0,0 +1,13 @@
+package com.study.springboottools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootToolsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootToolsApplication.class, args);
+  }
+
+}
