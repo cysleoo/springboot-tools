fc7655ee895bae6dd4ab32e748c103bc9baac748
ssliu <ssliu@chindigg.com>
2020-07-20 18:38:01 +0800

    feat: 添加gson测试代码

---
+++ src/main/java/com/study/springboottools/domain/json/Dog.java
@@ -0,0 +1,55 @@
+package com.study.springboottools.domain.json;
+
+import java.util.StringJoiner;
+
+/**
+ * @author ssliu
+ * @date 2020-07-20
+ */
+public class Dog {
+  private Integer age;
+
+
+
+  private String color;
+  private String type;
+
+  public Dog(Integer age, String color, String type) {
+    this.age = age;
+    this.color = color;
+    this.type = type;
+  }
+
+  public Integer getAge() {
+    return age;
+  }
+
+  public void setAge(Integer age) {
+    this.age = age;
+  }
+
+  public String getColor() {
+    return color;
+  }
+
+  public void setColor(String color) {
+    this.color = color;
+  }
+
+  public String getType() {
+    return type;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+  @Override
+  public String toString() {
+    return new StringJoiner(", ", Dog.class.getSimpleName() + "[", "]")
+        .add("age=" + age)
+        .add("color='" + color + "'")
+        .add("type='" + type + "'")
+        .toString();
+  }
+
+}
---
+++ src/test/java/com/study/springboottools/util/JsonUtilsTest.java
@@ -0,0 +1,92 @@
+package com.study.springboottools.util;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestTemplate;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.study.springboottools.domain.json.Dog;
+
+class JsonUtilsTest {
+
+  private Dog dog1;
+  private Dog dog2;
+  private List<Dog> dogList = new ArrayList<>();
+  private Gson gson;
+  private String str1 = "{\"age\":1,\"color\":\"yellow\",\"type\":\"husky\"}";
+  private String str2 =
+      "[{\"age\":1,\"color\":\"yellow\",\"type\":\"husky\"},{\"age\":2,\"color\":\"black\",\"type\":\"husky\"}]";
+
+  @BeforeEach
+  void setUp() {
+    gson = new Gson();
+    dog1 = new Dog(1, "yellow", "husky");
+    dog2 = new Dog(2, "black", "husky");
+    dogList.add(dog1);
+    dogList.add(dog2);
+  }
+
+  @Test
+  void obj2String() {
+    System.out.println(gson.toJson(dog1));
+    System.out.println(gson.toJson(dogList));
+  }
+
+  @Test
+  void string2Obj() {
+    Dog dog = gson.fromJson(str1, Dog.class);
+    assertThat(dog).isEqualToComparingFieldByField(dog1);
+  }
+
+  @Test
+  void obj2JsonElement() {
+    JsonObject obj = gson.toJsonTree(dog1).getAsJsonObject();
+    JsonArray array = gson.toJsonTree(dogList).getAsJsonArray();
+  }
+
+  @Test
+  void jsonObjectTest(){
+    JsonObject obj = new JsonObject();
+  // 增
+    obj.addProperty("data","value");
+    System.out.println(obj.toString());
+    //  查
+    System.out.println(obj.get("data"));
+    // 删
+    obj.remove("data");
+    System.out.println(obj.toString());
+  }
+
+  @Test
+  void jsonArrayTest(){
+    JsonArray array = new JsonArray();
+  //  1.增删改查
+    array.add(1);
+    array.add("test");
+    System.out.println(array.toString());
+    array.remove(0);
+    System.out.println(array.toString());
+    JsonObject jsonObject = new JsonObject();
+    jsonObject.addProperty("taa","test-new");
+    array.set(0,jsonObject);
+    System.out.println(array.toString());
+    System.out.println(array.get(0));
+
+  //  2.size、包含
+    System.out.println(array.size());
+    System.out.println(array.contains(jsonObject));
+
+  }
+
+
+
+}
